da45318fb6a352907fe0059d319be0a3a5c5c70e
bnasr <bijan.s.nasr@gmail.com>
2019-04-29 14:43:14 -0700

    drafted 2-stage model

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CDM
+
+This repo contains codes and document for a Contuous Development Model that can model ordinal phenology model.
--- explore.R
+++ explore.R
@@ -1,3 +1,7 @@
 load('data/h12v04_0002145419.rda')
 # load('data/h12v04_0002464709.rda')
 # load('data/h12v04_0004997729.rda')
+
+hist(rbinom(1000,size = 1, prob = 0.5))
+hist(rmultinom(1000,size = 1, prob = c(0.2, 0.8))[2,])
+
