3a2e295eb504db803806195d65a9b2493eefcb5f
bnasr <bijan.s.nasr@gmail.com>
2019-05-16 15:58:08 -0700

    binomial version

--- diagnostics.R
+++ diagnostics.R
@@ -1,3 +1,6 @@
+rm(list = ls())
+graphics.off()
+
 library(data.table)
 
 #load local functions
@@ -5,12 +8,15 @@ source('funcs.R')
 
 # simulate data
 
-beta = matrix(c(-0.5, 2.5))
-sigma <- 0
+beta = matrix(c(1, 3))
+sigma <- 0.01
 
-n = 100 # number of phenological cycles
+n = 50 # number of phenological cycles
 nd = 100 # number of days in each cycle
-h_onset <- 50
+# h_onset <- 50
+kappa = -40
+lambda = 1
+
 
 data = data.table(year = rep(1:n, each = nd), 
                   doy = rep(1:nd, times = n))
@@ -18,7 +24,7 @@ data = data.table(year = rep(1:n, each = nd),
 # set.seed(1)
 
 data[, temp := simulate_temp(doy, 
-                             tmin = runif(-20, 0, n = 1), 
+                             tmin = runif(-5, 0, n = 1), 
                              tmax = runif(20, 40, n = 1)), year]
 
 # data[,plot(temp)]
@@ -36,8 +42,14 @@ data[,h:=cumsum(dh), year]
 
 data[year==1,plot(h),]
 
-data[,onset:=(h>h_onset)*1]
+library(boot)
+data[,p:=inv.logit(kappa + lambda*h)]
 
+data[,onset:=rbinom(n = .N, size = 1, prob = p)]
+
+# data[,onset:=(h>h_onset)*1]
+data$onset
+data[,min(which(onset==1)), year]
 data[,min(which(onset==1)), year][,hist(V1)]
 Y = data$onset
 
@@ -46,28 +58,31 @@ library(R2jags)
 model <- jags(model.file = 'model/onset-model.bugs', 
              data = list(Y = Y, 
                          X = X, 
+                         kappa = kappa, 
+                         lambda = lambda,
                          np = ncol(X), 
-                         h_onset = h_onset,
+                         # h_onset = h_onset,
                          n = length(Y),
                          HeadNodes = which(data$doy==1),
                          MainNodes = which(data$doy!=1)),
              parameters.to.save = c('beta', 'h'), 
              n.chains = 1,
-             n.iter = 1000, 
-             n.burnin = 200
+             n.iter = 4000, 
+             n.burnin = 1000
              # inits = list(list(beta= c(0, .0)))
              )
 
 layout(matrix(c(1,2,3,4,5,5), nrow = 2))
+gibbs_beta <- model$BUGSoutput$sims.list$beta
 
-hist(model$BUGSoutput$sims.list$beta[,1], 100)
+hist(gibbs_beta[,1], 100, xlim = range(gibbs_beta[,1], beta[1]))
 abline(v = beta[1], col = 'red', lwd = 2)
-plot(model$BUGSoutput$sims.list$beta[,1])
+plot(model$BUGSoutput$sims.list$beta[,1], ylim = range(gibbs_beta[,1], beta[1]))
 abline(h = beta[1], col = 'red', lwd = 2)
 
-hist(model$BUGSoutput$sims.list$beta[,2], 100)
+hist(model$BUGSoutput$sims.list$beta[,2], 100, xlim = range(gibbs_beta[,2], beta[2]))
 abline(v = beta[2], col = 'red', lwd = 2)
-plot(model$BUGSoutput$sims.list$beta[,2])
+plot(model$BUGSoutput$sims.list$beta[,2], ylim = range(gibbs_beta[,2], beta[2]))
 abline(h = beta[2], col = 'red', lwd = 2)
 
 plot(colMeans(model$BUGSoutput$sims.list$h), data$h)
